1595efeb6d3df7e4c0bd6b1b3b52478f08f22ae3
belu82 <44766864+belu82@users.noreply.github.com>
2019-11-28 15:14:29 +0100

    Add project files.

---
+++ Threading1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading1
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            ThreadStart starter = new ThreadStart(counting);
+            Thread first = new Thread(starter);
+            Thread second = new Thread(starter);
+            first.Start();
+            second.Start();
+
+            first.Join();
+            second.Join();
+
+            Console.Read();
+        }
+
+        public static void counting()
+        {
+            
+            for(int i = 0; i<10; i++)
+            {
+                Console.WriteLine("Count: {0} - Thread Id: {1}", i, Thread.CurrentThread.ManagedThreadId);
+                int sleepfor = 1000;
+
+                Thread.Sleep(sleepfor);
+            }
+        }
+    }
+}
